708dd629b37d58bc1d6b1751ec8634b86b93e04d
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-05-10 15:02:11 +0300

    init

---
+++ app.js
@@ -0,0 +1,70 @@
+/**
+ * 1 atvaizduoti userius is api
+ * saraso pavidalu kuriame yra username ir email
+ *
+ * 1.1 gauti pilna useriu info is api i console log
+ *
+ * 1.2 atrinkti tik reikalingus duomenis ir atvaizduoti saraso pavidalu
+ *
+ *
+ * 2 saraso elementai turi butipaspaudziami ir paspaudus mes
+ * atvaizduosim visa userio informacija puslapio desineje
+ *
+ * 2.1 gauti paspausto userio id
+ *
+ * 2.2 atlikti nauja fetch requesta i api ir gauti useri su nurodytu id
+ *
+ *
+ *
+ * 3 parisiusti todo objektus nuo 41 iki 57
+ * https://jsonplaceholder.typicode.com/todos
+ * kiekvienam prideti po papildoma savybe "letterCount" kuri nusako kiek
+ * raidziu yra tame todo
+ *
+ * 3.1  gautus todo isrikiuoti pagal atliktu ir neatliktu statusa
+ * // neatlikti rodomi pirmi
+ *
+ * 3.2 suskaiciuoti kiek todo yra atliktu kiek ne, grazinti atsakyma
+ * objekto pavidalu pvz: { done: 45, notDone: 34 }
+ *
+ *
+ * 4 susikurti atskira html faila gallery.html
+ *
+ * 4.1 atvaizduoti 9 paveikslelius  po 3 per eilute su antraste po paveiksleliu
+ * paveiksleliu traukiam is https://jsonplaceholder.typicode.com/photos
+ *
+ * 4.2 tureti mygtuka load more kuris uzkrauna papildomai dar po viena eile
+ * paveiksleliu. uzkrauti ir prideti tik naujus neperrasant jau esanciu
+ *
+ * 4.3 padaryti mygtuka 'reset' kuris paliktu galerijoj tik pirmus
+ * 9 paveikslelius
+ *
+ * 4.4 padaryti mygtuka kuris paslepia visus pavadinmus po paveiksleliais
+ * 'pic only' btn
+ *
+ *
+ *
+ *
+ *
+ */
+let num = "green";
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((resp) => resp.json())
+  .then((data) => {
+    // cia jau turim data
+    console.log(data);
+
+    filterData(data);
+    num = data;
+  })
+  .catch((err) => console.log(err));
+
+num; // 'green'
+
+function filterData(data) {
+  console.log(data);
+  let str = data.reduce((htmlStr, obj) => {
+    return (htmlStr += `<li>${obj.username}, ${obj.email} </li>`);
+  }, "");
+  console.log(str);
+}
