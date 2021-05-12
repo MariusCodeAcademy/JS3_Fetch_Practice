92200c9cfd4c54be39f58f1023f1d57699b2cb6d
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-05-12 14:48:58 +0300

    load2

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -3,27 +3,33 @@
 console.log("gallery");
 
 const loadBtn = document.getElementById("btn");
-
+let lastId = 9;
 let first50;
 // gauti duomenis
 fetch("https://jsonplaceholder.typicode.com/photos")
   .then((resp) => resp.json())
   .then((data) => {
-    first50 = data;
+    first50 = data.splice(0, 50);
     console.log(first50);
-    generateImagesOnHtml(first50.splice(0, 9));
+    generateImagesOnHtml(first50.filter((o) => o.id <= 9));
   })
   .catch((err) => console.log(err));
 
 // gauti pirmus 9
 // gaunam po 3
-// 10 - 12, 15 -
+// 10 - 12,  // 9 + 1 - 9 + 3
+// 13 - 15,  // 12 +1 - 12 + 3
+// 16 - 18
 
 function generateImagesOnHtml(data) {
-  console.log("generating", data);
+  //   console.log("generating", data);
+  console.table(data);
 }
 
 loadBtn.addEventListener("click", () => {
-  console.log("click");
-  generateImagesOnHtml(first50.splice(0, 3));
+  //                                                  9                   12
+  generateImagesOnHtml(first50.filter((o) => o.id > lastId && o.id <= lastId + 3));
+  // 9 + 3 = 12
+  lastId += 3;
+  //   generateImagesOnHtml(first50.filter(o => o.id > 12 && o.id <= 15));
 });
